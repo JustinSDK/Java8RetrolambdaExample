acb6f35f18c1094466112d187ab6715f0b9c328e
Justin Lin <caterpillar@openhome.cc>
2015-04-20 14:59:41 +0800

    提取點數計算

--- src/example/Customer.java
+++ src/example/Customer.java
@@ -28,12 +28,7 @@ public class Customer {
             
             double thisAmount = each.getCharge();
 
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) {
-                frequentRenterPoints++;
-            }
+            frequentRenterPoints += getFrequentRenterPoints(each);
 
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
@@ -44,6 +39,13 @@ public class Customer {
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return result;
+    }
+
+    private int getFrequentRenterPoints(Rental each) {
+        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) {
+            return 2;
+        }
+        return 1;
     }
 
 
